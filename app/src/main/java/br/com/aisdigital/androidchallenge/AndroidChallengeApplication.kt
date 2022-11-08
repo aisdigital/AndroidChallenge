f1006dc973758a648c871d8df2da539d61b35dc6
Agnaldo Rodrigues de Carvalho <agnaldordcn@gmail.com>
2022-11-08 00:11:47 -0300

    Adds koin dependency
    Signed-off-by: Agnaldo Rodrigues de Carvalho <agnaldordcn@gmail.com>

--- app/src/main/java/br/com/aisdigital/androidchallenge/AndroidChallengeApplication.kt
+++ app/src/main/java/br/com/aisdigital/androidchallenge/AndroidChallengeApplication.kt
@@ -1,4 +1,29 @@
 package br.com.aisdigital.androidchallenge
 
-class AndroidChallengeApplication {
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext.startKoin
+import org.koin.dsl.module
+
+class AndroidChallengeApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        setupKoin()
+    }
+
+    private fun setupKoin() {
+        val module = module {
+
+        }
+        startKoin {
+            // Log Koin into Android logger
+            androidLogger()
+            // Reference Android context
+            androidContext(this@AndroidChallengeApplication)
+            // Load modules
+            modules(module)
+        }
+    }
 }
